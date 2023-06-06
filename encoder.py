b9e695938ebb28d637806de2144bd4e16910ac0f
nikitaromanoov <91135334+nikitaromanoov@users.noreply.github.com>
2023-06-06 23:19:07 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -65,4 +65,4 @@ print(dec_img.shape)
 print(type(dec_img))
 print(dec_img)
 with open(args.path_result, "w") as w:
-    w.write(json.dumps(list(dec_img[0])))
+    w.write(json.dumps(dec_img.tolist()))
